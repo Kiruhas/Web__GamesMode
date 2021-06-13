0fae1d6e11c58db22d663eac5635494c3d072e85
kiruhas <kiruhas46@gmail.com>
2021-06-13 14:45:30 +0300

    Version 13.06.2021

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const openNav = $('.nav__open');
+const nav = $('.nav');
+const closeNav = $('.nav__close');
+
+openNav.on('click', ()=> {
+  $('.nav').addClass('is-active');
+})
+
+closeNav.on('click', ()=> {
+  $('.nav').removeClass('is-active');
+})
+
+$('#js-carousel-1').each(function () {
+  // Создаем карусель
+  var owl = $(this).find('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1
+      },
+      450: {
+        items: 2
+      },
+      900: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      },
+      1400: {
+        items: 5
+      },
+    },
+  });
+
+  // При клике по кнопке Влево
+  $(this).find('.js-prev').on('click', function () {
+    // Перематываем карусель назад
+    owl.trigger('prev.owl.carousel');
+  });
+
+  // При клике по кнопке Вправо
+  $(this).find('.js-next').on('click', function () {
+    // Перематываем карусель вперед
+    owl.trigger('next.owl.carousel');
+  });
+});
